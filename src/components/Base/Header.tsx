a8471ccdb343167ac7c029d486380008254c63ad
js2102 <ssen199504@gmail.com>
2022-12-22 18:06:18 +0900

    Apply styled-components

--- src/components/Base/Header.tsx
+++ src/components/Base/Header.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Component = {
+  Wrapper: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 24px;
+  `,
+  Logo: styled.div`
+    font-size: 32px;
+    padding: 8px;
+  `,
+  Login: styled.div``
+}
 
 const Header = () => {
   return (
-    <div className='header' style={{ display: "flex", justifyContent: "space-between", alignItems: "center", margin: "0 24px"}}>
-      <div style={{ fontSize: "32px", padding: "8px" }}>런치피커</div>
-      <div>로그인</div>
-    </div>
+    <Component.Wrapper>
+      <Component.Logo>런치피커</Component.Logo>
+      <Component.Login>로그인</Component.Login>
+    </Component.Wrapper>
   )
 };
 
